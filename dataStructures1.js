603a754dde135b2edb2ea6abb2fc0c75be592943
Kara Cavanaugh <karacav@msn.com>
2019-09-28 21:27:52 -0400

    Through number 4

--- dataStructures1.js
+++ dataStructures1.js
@@ -1,2 +1,22 @@
 // 1. Use an Array to Store a Collection of Data
 let yourArray = [6, "six", false, "the length needs", " to be 5"];
+
+// 2. Access an Array's Contents Using Bracket Notation
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "not B";
+console.log(myArray);
+
+// 3. Arr.push() and Arr.unshift()
+function mixedNumbers(arr) {
+    arr.unshift("I", 2, 'three');
+    arr.push(7,'VIII', 9);
+    return arr;
+}
+console.log(mixedNumbers(['IV', 5, 'six']));
+
+// 4. Remove Items from an Array with pop() and shift()
+function popShift(arr) {
+  let popped = arr.pop();
+  let shifted = arr.shift();
+  return [shifted, popped];
+}
