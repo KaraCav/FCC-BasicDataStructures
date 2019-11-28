45108d9ff5b2d8d1ee7c0215008dc014d71b9c4e
Kara Cavanaugh <karacav@msn.com>
2019-11-27 22:38:45 -0500

    Through number 15

---
+++ dataStructures2.js
@@ -0,0 +1,12 @@
+// 14. Generate Random Fractions with JavaScript
+function randomFraction() {
+ return Math.random();
+}
+
+// 15. Create complex multi-dimensional arrays
+let myNestedArray = [
+  ['unshift', false, 1, 2, 3, 'complex', 'nested', ['deep', ['deeper',[3,'hat','deepest']]]],
+  ['loop', 'shift', 6, 7, 1000, 'method'],
+  ['concat', false, true, 'spread', 'array'],
+  ['mutate', 1327.98, 'splice', 'slice', 'push']
+];
