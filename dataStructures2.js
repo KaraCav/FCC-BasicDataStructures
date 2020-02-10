af96a3f4d1e4937e46adc3178435ec82263d5f64
Kara Cavanaugh <karacav@msn.com>
2020-02-10 13:04:10 -0500

    Through number 17

--- dataStructures2.js
+++ dataStructures2.js
@@ -10,3 +10,24 @@ let myNestedArray = [
   ['concat', false, true, 'spread', 'array'],
   ['mutate', 1327.98, 'splice', 'slice', 'push']
 ];
+
+// 16. Add items to an object
+let foods = {
+  apples: 25,
+  plums: 28
+};
+foods.bananas = 13;
+foods["strawberries"]= 27;
+console.log(foods);
+
+// 17. Modify an Object Nested Within an Object
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+userActivity.data.online = 45;
+console.log(userActivity);
